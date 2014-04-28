8acfde8cc4788f4a6fba6d0a093d7dc265d65718
Philipp Stucki <philipp@qwert.ch>
2014-04-29 00:45:15 +0200

    adds more examples and images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')(),
     paths = {
-        examples: 'examples/src/ex-*.html'
+        examples: 'examples/src/ex-*.html',
+        dotgraphs: 'images/src/*.gv.txt'
     };
+
+gulp.task('dotgraphs', function() {
+    return gulp.src(paths.dotgraphs)
+    .pipe(plugins.spawn({
+        cmd: 'dot',
+        args: ['-Tsvg'],
+        filename: function(base, ext) {
+            return base + '.svg';
+        }
+    }))
+    .pipe(gulp.dest('images/'));
+});
 
 gulp.task('examples', function() {
     return gulp.src(paths.examples)
@@ -12,6 +25,14 @@ gulp.task('examples', function() {
     .pipe(gulp.dest('examples/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch-examples', ['examples'], function() {
     gulp.watch([paths.examples,'examples/src/layout.html'], ['examples']);
 });
+
+gulp.task('watch-dotgraphs', ['dotgraphs'], function() {
+    gulp.watch([paths.dotgraphs], ['dotgraphs']);
+});
+
+gulp.task('build', ['examples', 'dotgraphs'], function() {
+});
+
